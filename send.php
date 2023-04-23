cd2c489a32af1caeeb676a3d8e78d290f64dc44e
KennethDV <deverakenneth21@gmail.com>
2023-04-24 04:11:36 +0800

    new add

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -7,29 +8,32 @@ require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
 if(isset($_POST["send"])){
-    
-    
+    //POST
+    $sender = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    //PHP Mailer Declaration
     $mail = new PHPMailer(true);
-    
+
     $mail->isSMTP();
-    $mail->Host = 'smtp.hostinger.com';
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'kenneth@villamasanori.fun';
-    $mail->Password = 'Kenneth@123';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-    
-    $mail->setFrom('kenneth@villamasanori.fun');
-    
-    $mail->addAddress($_POST["email"]);
-    
+    $mail->Username = 'kenneth@villamasanori';
+    $mail->Password = 'clkbqhdlrpcnjzsw'; //Email Password
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = '465';
+
+    //SETTING Email
+    $mail->setFrom('buenaroa.connect@gmail.com', 'Buenaroa'); //Senders Email
+    $mail->addAddress($sender); //Receivers Email
     $mail->isHTML(true);
-    
-    $mail->Subject = $_POST["subject"];
-    $mail->Body = "Thank you for contacting us! We will get back to you shortly. Have a wonderful day " . $_POST['name'];
-    
+    $mail->Subject = "Good Day!";
+    $mail->Body = "Thank you" . $name . ", We have received your email. Please wait for our response.";
     $mail->send();
-    
+
     header('Location: index.php');
 }
+
 ?>
