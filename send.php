5765a093dc6da901f5a53968161916bb576f5f9e
KennethDV <deverakenneth21@gmail.com>
2023-04-28 00:57:22 +0800

    sana fix

--- send.php
+++ send.php
@@ -8,32 +8,28 @@ require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
 if(isset($_POST["send"])){
-    //POST
-    $sender = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    //PHP Mailer Declaration
+ 
     $mail = new PHPMailer(true);
-
+    
     $mail->isSMTP();
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'kenneth@villamasanori.fun';
-    $mail->Password = 'Kenneth@123'; //Email Password
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = '465';
-
-    //SETTING Email
-    $mail->setFrom('kenneth@villamasanori.fun', 'Dental Clique'); //Senders Email
-    $mail->addAddress($sender); //Receivers Email
+    $mail->Password = 'Kenneth@123';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    
+    $mail->setFrom('kenneth@villamasanori.fun');
+    
+    $mail->addAddress($_POST["email"]);
+    
     $mail->isHTML(true);
-    $mail->Subject = "Good Day!";
-    $mail->Body = "Thank you" . $name . ", We have received your email. Please wait for our response.";
+    
+    $mail->Subject = $_POST["subject"];
+    $mail->Body = "Thank you for contacting us! We will get back to you shortly. Have a wonderful day " . $_POST['name'];
+    
     $mail->send();
-
+    
     header('Location: index.php');
 }
-
 ?>
